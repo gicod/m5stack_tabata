0971e550f058bcf6d39ce7595801e29649c5bf22
IvanGor <jaguarus34@gmail.com>
2025-11-26 21:28:35 +0300

    уменьшил мерцание экрана

--- src/config.h
+++ src/config.h
@@ -1,19 +1,19 @@
 #pragma once
 
 // WiFi credentials
-const char* const WIFI_SSID = "Xiaomi_217C";
-const char* const WIFI_PASS = "9264300695";
+const char *const WIFI_SSID = "Xiaomi_217C";
+const char *const WIFI_PASS = "9264300695";
 
 // Tabata settings
-constexpr uint8_t TABATA_ROUNDS = 8;
-constexpr uint16_t WORK_TIME_MS = 20000;    // 20 sec
-constexpr uint16_t REST_TIME_MS = 10000;     // 10 sec
+constexpr int TABATA_ROUNDS = 4;
+constexpr int WORK_TIME_S = 10;  // 20 sec
+constexpr int RELAX_TIME_S = 5; // 10 sec
 
 // Display settings
 constexpr uint16_t BACKGROUND_COLOR = BLACK;
 constexpr uint16_t TEXT_COLOR = WHITE;
 constexpr uint16_t WORK_COLOR = RED;
-constexpr uint16_t REST_COLOR = GREEN;
+constexpr uint16_t RELAX_COLOR = GREEN;
 
 // Buzzer
 constexpr uint8_t BUZZER_PIN = 2;
